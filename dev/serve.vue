71ea2a6b36da0d50bdc865430262e0e479fa1bf6
Jose <josemaria@hashed.io>
2022-05-15 21:24:38 -0500

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { JmgayossoIpfsComponentsVueSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  JmgayossoIpfsComponentsVueSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <jmgayosso-ipfs-components-vue-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as JmgayossoIpfsComponentsVueSample } from './jmgayosso-ipfs-components-vue-sample.vue';
